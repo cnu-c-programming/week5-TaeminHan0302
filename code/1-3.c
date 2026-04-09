c4f4004cb6295a6339baf4c46e0b023a172e5e8e
TaeminHan0302 <tefzen0302@gmail.com>
2026-04-09 22:27:50 +0900

    Update 1-3.c

--- code/1-3.c
+++ code/1-3.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
 void print_reverse(int *arr, int size) {
+    int *p = arr + size - 1;
+    while (p >= arr) {
+        printf("%d", *p);
+        if (p > arr) {
+            printf(" ");
+        }
+        p--;
+    }
 
 }
 
